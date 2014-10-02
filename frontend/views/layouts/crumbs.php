25e645799564aff7932f8f3c42629a0b20625272
straysh <jobhancao@gmail.com>
2014-10-02 14:09:26 +0800

    .

--- frontend/views/layouts/crumbs.php
+++ frontend/views/layouts/crumbs.php
@@ -7,7 +7,7 @@ __Straysh的个人博客__ » [首页][1] » [{$category->nav_name}][2]
 [1]:http://www.straysh.com "Straysh的个人博客"
 [2]:{$categoryUrl} "{$category->nav_name}"
 
-<form action="/search" class="header-search pull-left hidden-sm hidden-xs" onclick="alert('not implemented yet!');">
+<form action="/search" class="header-search pull-left hidden-sm hidden-xs" onclick="alert('not implemented yet!');return false;">
     <button type="submit" class="btn btn-link"><span class="sr-only">搜索</span><span class="glyphicon glyphicon-search"></span></button>
     <input id="searchBox" name="q" type="text" placeholder="输入关键字搜索" class="form-control" value="" style="width: 180px;">
 </form>
