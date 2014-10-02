db62ba33dda53103b589308f034ea1e429704c1a
straysh <jobhancao@gmail.com>
2014-10-02 23:42:16 +0800

    .

--- frontend/www/js/main.js
+++ frontend/www/js/main.js
@@ -1,12 +1,14 @@
 require.config({
     baseUrl: '/js',
     paths: {
-        'jquery': 'libs/jquery-1.9.0'
+        jquery: 'libs/jquery-1.9.0'
     }
 });
 
 require(['jquery', 'libs/highlight.pack', 'disqus', 'back2top'], function($, hljs, disqus, back2top){
-    hljs.initHighlightingOnLoad();
-    back2top();
-    disqus();
+    $().ready(function(){
+        hljs.initHighlightingOnLoad();
+        back2top();
+        disqus();
+    });
 });
